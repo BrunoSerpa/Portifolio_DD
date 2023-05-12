2613f07efa49565b44612b2a247f1778850434b9
BrunoSerpa <serpabruno2005@gmail.com>
2023-05-12 15:26:47 -0300

    troquei route index para sobre

--- src/app.py
+++ src/app.py
@@ -7,20 +7,20 @@ def home():
     title='Bem vindo!!'
     return render_template("index.html", title=title, nav=nav)
 
-@app.route("/index")
-def index():
+@app.route("/sobre")
+def sobre():
     nav=['href=/trabalhos','Trabalhos',' ','Sobre mim', 'href=/contatos','Contato']
     title='Quem Sou'
     return render_template("index.html", title=title, nav=nav)
 
 @app.route("/trabalhos")
 def trabalhos():
-    nav=['href=/index','Sobre mim', ' ','Trabalhos','href=/contatos','Contato']
+    nav=['href=/sobre','Sobre mim', ' ','Trabalhos','href=/contatos','Contato']
     title="Trabalhos"
     return render_template("trabalhos.html", title=title, nav=nav)
 
 @app.route("/contatos")
 def contatos():
-    nav=['href=/trabalhos','Trabalhos', '', 'Contato','href=/index','Sobre mim']
+    nav=['href=/trabalhos','Trabalhos', '', 'Contato','href=/sobre','Sobre mim']
     title='Contatos'
     return render_template("contatos.html", title=title, nav=nav)

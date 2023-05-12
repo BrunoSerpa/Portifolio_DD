755134cc4a4eda34a7933a4d6b9611b6c520c6fd
BrunoSerpa <serpabruno2005@gmail.com>
2023-05-12 15:16:53 -0300

    add nav

--- src/app.py
+++ src/app.py
@@ -3,20 +3,24 @@ app = Flask("__name__")
 
 @app.route("/")
 def home():
+    nav=['href=/trabalhos','Trabalhos',' ','Sobre mim', 'href=/contatos','Contato']
     title='Bem vindo!!'
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, nav=nav)
 
 @app.route("/index")
 def index():
+    nav=['href=/trabalhos','Trabalhos',' ','Sobre mim', 'href=/contatos','Contato']
     title='Quem Sou'
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, nav=nav)
 
 @app.route("/trabalhos")
-def quemsomos():
+def trabalhos():
+    nav=['href=/index','Sobre mim', ' ','Trabalhos','href=/contatos','Contato']
     title="Trabalhos"
-    return render_template("trabalhos.html", title=title)
+    return render_template("trabalhos.html", title=title, nav=nav)
 
 @app.route("/contatos")
 def contatos():
+    nav=['href=/trabalhos','Trabalhos', '', 'Contato','href=/index','Sobre mim']
     title='Contatos'
-    return render_template("contatos.html", title=title)
+    return render_template("contatos.html", title=title, nav=nav)

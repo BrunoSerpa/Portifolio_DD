350c1140ece194a758e598bac847d1f7489da7e9
BrunoSerpa <serpabruno2005@gmail.com>
2023-06-19 09:38:50 -0300

    Arrumei pontos abordados na apresentação do Portfolio

--- src/app.py
+++ src/app.py
@@ -3,25 +3,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    nav=['href=/trabalhos','Trabalhos',' ','Sobre mim', 'href=/contatos','Contato']
+    nav=['href=/trabalhos','Trabalhos',' ','Home', 'href=/contatos','Contato']
     title='Bem vindo!!'
     return render_template("index.html", title=title, nav=nav, ensinoJson=fileJson('ensino'))
 
 @app.route("/sobre")
 def sobre():
-    nav=['href=/trabalhos','Trabalhos',' ','Sobre mim', 'href=/contatos','Contato']
+    nav=['href=/trabalhos','Trabalhos',' ','Home', 'href=/contatos','Contato']
     title='Quem Sou'
     return render_template("index.html", title=title, nav=nav, ensinoJson=fileJson('ensino'))
 
 @app.route("/trabalhos")
 def trabalhos():
-    nav=['href=/sobre','Sobre mim', ' ','Trabalhos','href=/contatos','Contato']
+    nav=['href=/sobre','Home', ' ','Trabalhos','href=/contatos','Contato']
     title="Trabalhos"
     return render_template("trabalhos.html", title=title, nav=nav, trabalhoJson=fileJson('trabalhos'), voluntariadoJson=fileJson('voluntariados'), projetoJson=fileJson('projetos'))
 
 @app.route("/contatos")
 def contatos():
-    nav=['href=/trabalhos','Trabalhos', '', 'Contato','href=/sobre','Sobre mim']
+    nav=['href=/trabalhos','Home', '', 'Contato','href=/sobre','Sobre mim']
     title='Contatos'
     return render_template("contatos.html", title=title, nav=nav)
 
